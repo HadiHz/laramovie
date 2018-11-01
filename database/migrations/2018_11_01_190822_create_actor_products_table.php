d95596e5a76efda3f19971f986e939ba2d2b730b
mohammadmohammadinia <mohamadmohamadi230@gmail.com>
2018-11-01 23:15:45 +0330

    migrations

--- database/migrations/2018_11_01_190822_create_actor_products_table.php
+++ database/migrations/2018_11_01_190822_create_actor_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateActorProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('actor_products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('actor_id');
+            $table->integer('producible_id');
+            $table->integer('producible_type');
+            $table->primary(['actor_id' , 'producible_id' ,'producible_type'] ,'primary_key');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('actor_products');
     }
 }
--- database/migrations/2018_11_01_190833_create_genre_products_table.php
+++ database/migrations/2018_11_01_190833_create_genre_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGenreProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genre_products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('actor_id');
+            $table->integer('producible_id');
+            $table->integer('producible_type');
+            $table->primary(['genre_id' , 'producible_id' ,'producible_type'] ,'primary_key');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genre_products');
     }
 }
