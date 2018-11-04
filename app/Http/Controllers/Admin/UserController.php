678e311459746d35ed41e717f8d9bbaf2253b144
mohammadmohammadinia <mohamadmohamadi230@gmail.com>
2018-11-04 15:00:15 +0330

    comment

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 11/3/2018
+ * Time: 12:03 AM
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+
+}
